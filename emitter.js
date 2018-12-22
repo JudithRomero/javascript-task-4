2852ea94d8e58735c492824e87d6621a529a95b3
JudithRomero <43612189+JudithRomero@users.noreply.github.com>
2018-12-23 00:29:43 +0500

    Update emitter.js

--- emitter.js
+++ emitter.js
@@ -6,11 +6,26 @@
  */
 const isStar = true;
 
+
 /**
  * Возвращает новый emitter
  * @returns {Object}
  */
-function getEmitter() {
+const getEmitter = () => {
+    const root = {};
+    const traverse = (event) => {
+        const visited = [];
+        event.split('.').reduce((acc, x) => {
+            acc[x] = acc[x] || {};
+            acc[x]['.'] = acc[x]['.'] || [];
+            visited.push(acc[x]);
+
+            return acc[x];
+        }, root);
+
+        return visited;
+    };
+
     return {
 
         /**
@@ -18,26 +33,43 @@ function getEmitter() {
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Object} emitter
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            const node = traverse(event).pop();
+            node['.'].push([context, handler]);
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object} emitter
          */
         off: function (event, context) {
-            console.info(event, context);
+            const node = traverse(event).pop();
+            for (let child of Object.keys(node).filter(x => x !== '.')) {
+                delete node[child];
+            }
+            node['.'] = node['.'].filter(x => x[0] !== context);
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object} emitter
          */
         emit: function (event) {
-            console.info(event);
+            const nodes = traverse(event).reverse();
+            nodes.forEach(n => n['.'].forEach(
+                ([context, handler]) => handler.bind(context)()
+            ));
+
+            return this;
         },
 
         /**
@@ -47,9 +79,17 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} times – сколько раз получить уведомление
+         * @returns {Object} emitter
          */
         several: function (event, context, handler, times) {
-            console.info(event, context, handler, times);
+            function severalHandler() {
+                if (times-- > 0) {
+                    handler.bind(this)();
+                }
+            }
+            this.on(event, context, severalHandler);
+
+            return this;
         },
 
         /**
@@ -59,15 +99,23 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} frequency – как часто уведомлять
+         * @returns {Object} emitter
          */
         through: function (event, context, handler, frequency) {
-            console.info(event, context, handler, frequency);
+            let times = 0;
+            function throughHandler() {
+                if (!(times++ % frequency)) {
+                    handler.bind(this)();
+                }
+            }
+            this.on(event, context, throughHandler);
+
+            return this;
         }
     };
-}
+};
 
 module.exports = {
     getEmitter,
-
     isStar
 };
